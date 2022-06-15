a7b84c9c27340fc6e987f6b95d886e4e5e8bbe21
EliveltonRepolho <elivelton.repolho@gmail.com>
2022-06-15 01:52:57 -0300

    test

--- entrypoint-gitlab-runner copy.sh
+++ entrypoint-gitlab-runner copy.sh
@@ -11,6 +11,15 @@ function gitlab_unregister {
 
 trap gitlab_unregister EXIT SIGHUP SIGINT SIGTERM
 
+# Define runner tags
+# https://gitlab.com/gitlab-org/charts/gitlab-runner/-/blob/main/values.yaml#L203
+if [ -n "${RUNNER_TAG_LIST:-}" ]
+then
+    RUNNER_TAG_LIST_OPT=("tags = $RUNNER_TAG_LIST")
+else
+    RUNNER_TAG_LIST_OPT=("runUntagged = true")
+fi
+
 TEMPLATE_FILE='./template-config.toml'
 # Create config.toml template file
 # https://docs.gitlab.com/runner/configuration/advanced-configuration.html#the-runnersmachine-section
@@ -21,25 +30,20 @@ concurrent = ${RUNNER_CONCURRENT_LIMIT}
 check_interval = 0
 
 [[runners]]
-  name = "echope-erp-gitlab-runner-general"
+  name = "echope-erp-gitlab-runner"
   description = "Gitlab Runner executing Pipeline Jobs in EC2" 
   executor = "docker+machine"
   limit = ${RUNNER_CONCURRENT_LIMIT}
-  request_concurrency = ${RUNNER_CONCURRENT_LIMIT}
-  environment = [
-    "DOCKER_DRIVER=overlay2",
-    "DOCKER_TLS_CERTDIR="
-  ]
-  runUntagged = true
+  environment = ["DOCKER_DRIVER=overlay2", "DOCKER_TLS_CERTDIR="]
+  ${RUNNER_TAG_LIST_OPT}
   [runners.docker]
     privileged = true
     disable_cache = true
     tls_verify = true
   [runners.machine]
     IdleTime = 60
-    MaxBuilds = 10 # We delete the VM after N jobs has finished so we can try to evict running out of space (disk).
     MachineDriver = "amazonec2"
-    MachineName = "gitlab-docker-machine-general-%s"
+    MachineName = "gitlab-docker-machine-%s"
     MachineOptions = [
       "amazonec2-ami=${AWS_AMI}",
       "amazonec2-root-size=${AWS_ROOT_SIZE}",
@@ -50,7 +54,7 @@ check_interval = 0
       "amazonec2-use-private-address=true",
       "amazonec2-ssh-user=${AWS_SSH_USER}",
       "amazonec2-security-group=${AWS_SECURITY_GROUP}",
-      "amazonec2-instance-type=${AWS_INSTANCE_TYPE_GENERAL}",
+      "amazonec2-instance-type=${AWS_INSTANCE_TYPE}",
       "amazonec2-tags=stack,echope-erp,stack-env,echope-erp-infra-devops,stack-group,echope-erp-gitlab-ec2-runner",
     ]
   [runners.cache]
@@ -62,56 +66,12 @@ check_interval = 0
       SecretKey = "${CACHE_S3_SECRET_KEY}"
       BucketName = "${CACHE_S3_BUCKET_NAME}"
       BucketLocation = "${CACHE_S3_BUCKET_LOCATION}"
-
-[[runners]]
-  name = "echope-erp-gitlab-runner-integration-tests"
-  description = "Gitlab Runner executing Pipeline Jobs in EC2 with Integration Tests configuration" 
-  executor = "docker+machine"
-  limit = ${RUNNER_CONCURRENT_LIMIT}
-  request_concurrency = ${RUNNER_CONCURRENT_LIMIT}
-  environment = [
-    "DOCKER_DRIVER=overlay2",
-    "DOCKER_TLS_CERTDIR="
-  ]
-  tags = "tests-integration"
-  [runners.docker]
-    privileged = true
-    disable_cache = true
-    tls_verify = true
-  [runners.machine]
-    IdleTime = 60
-    MaxBuilds = 10 # We delete the VM after N jobs has finished so we can try to evict running out of space (disk).
-    MachineDriver = "amazonec2"
-    MachineName = "gitlab-docker-machine-it-%s"
-    MachineOptions = [
-      "amazonec2-ami=${AWS_AMI}",
-      "amazonec2-root-size=${AWS_ROOT_SIZE}",
-      "amazonec2-region=${AWS_DEFAULT_REGION}",
-      "amazonec2-vpc-id=${AWS_VPC_ID}",
-      "amazonec2-subnet-id=${AWS_SUBNET_ID}",
-      "amazonec2-zone=${AWS_SUBNET_ZONE}",
-      "amazonec2-use-private-address=true",
-      "amazonec2-ssh-user=${AWS_SSH_USER}",
-      "amazonec2-security-group=${AWS_SECURITY_GROUP}",
-      "amazonec2-instance-type=${AWS_INSTANCE_TYPE_IT}",
-      "amazonec2-tags=stack,echope-erp,stack-env,echope-erp-infra-devops,stack-group,echope-erp-gitlab-ec2-runner-it",
-    ]
-  [runners.cache]
-    Type = "${CACHE_TYPE}"
-    Shared = ${CACHE_SHARED}
-    [runners.cache.s3]
-      ServerAddress = "${CACHE_S3_SERVER_ADDRESS}"
-      AccessKey = "${CACHE_S3_ACCESS_KEY}"
-      SecretKey = "${CACHE_S3_SECRET_KEY}"
-      BucketName = "${CACHE_S3_BUCKET_NAME}"
-      BucketLocation = "${CACHE_S3_BUCKET_LOCATION}"
-
 EOF
 
 echo "Registering runner using config.toml template file: $TEMPLATE_FILE"
 # https://docs.gitlab.com/runner/commands/
 # --debug
-gitlab-runner --debug register \
+gitlab-runner register \
 --template-config $TEMPLATE_FILE \
 --non-interactive
 
