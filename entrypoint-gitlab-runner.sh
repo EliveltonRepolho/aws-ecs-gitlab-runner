9125767969d1ead7d465fe3b4a9c13c5bd34fac7
EliveltonRepolho <elivelton.repolho@gmail.com>
2022-10-27 02:07:11 -0300

    wip

--- entrypoint-gitlab-runner.sh
+++ entrypoint-gitlab-runner.sh
@@ -87,51 +87,6 @@ cat <<EOF >$TEMPLATE_FILE_GENERAL
       BucketLocation = "${CACHE_S3_BUCKET_LOCATION}"
 EOF
 
-TEMPLATE_FILE_IT='./template-it-config.toml'
-cat <<EOF >$TEMPLATE_FILE_IT
-[[runners]]
-  name = "echope-erp-gitlab-runner-integration-tests"
-  description = "Gitlab Runner executing Pipeline Jobs in EC2 with Integration Tests configuration" 
-  executor = "docker+machine"
-  limit = ${RUNNER_CONCURRENT_LIMIT}
-  request_concurrency = ${RUNNER_CONCURRENT_LIMIT}
-  environment = [
-    "DOCKER_DRIVER=overlay2",
-    "DOCKER_TLS_CERTDIR="
-  ]
-  [runners.docker]
-    privileged = true
-    disable_cache = true
-    tls_verify = true
-  [runners.machine]
-    IdleTime = 60
-    MaxBuilds = 10 # We delete the VM after N jobs has finished so we can try to evict running out of space (disk).
-    MachineDriver = "amazonec2"
-    MachineName = "gitlab-it-%s"
-    MachineOptions = [
-      "amazonec2-ami=${AWS_AMI}",
-      "amazonec2-root-size=${AWS_ROOT_SIZE}",
-      "amazonec2-region=${AWS_DEFAULT_REGION}",
-      "amazonec2-vpc-id=${AWS_VPC_ID}",
-      "amazonec2-subnet-id=${AWS_SUBNET_ID}",
-      "amazonec2-zone=${AWS_SUBNET_ZONE}",
-      "amazonec2-use-private-address=true",
-      "amazonec2-ssh-user=${AWS_SSH_USER}",
-      "amazonec2-security-group=${AWS_SECURITY_GROUP}",
-      "amazonec2-instance-type=${AWS_INSTANCE_TYPE_IT}",
-      "amazonec2-tags=stack,echope-erp,stack-env,echope-erp-infra-devops,stack-group,echope-erp-gitlab-ec2-runner-it",
-    ]
-  [runners.cache]
-    Type = "${CACHE_TYPE}"
-    Shared = ${CACHE_SHARED}
-    [runners.cache.s3]
-      ServerAddress = "${CACHE_S3_SERVER_ADDRESS}"
-      AccessKey = "${CACHE_S3_ACCESS_KEY}"
-      SecretKey = "${CACHE_S3_SECRET_KEY}"
-      BucketName = "${CACHE_S3_BUCKET_NAME}"
-      BucketLocation = "${CACHE_S3_BUCKET_LOCATION}"
-EOF
-
 echo "Registering runner using config.toml template file: $TEMPLATE_FILE_GENERAL"
 
 # --debug
@@ -139,12 +94,6 @@ gitlab-runner --debug register \
 --template-config $TEMPLATE_FILE_GENERAL \
 --non-interactive \
 --run-untagged
-
-echo "Registering runner using config.toml template file: $TEMPLATE_FILE_IT"
-gitlab-runner --debug register \
---template-config $TEMPLATE_FILE_IT \
---non-interactive \
---tag-list "test:integration:browser"
 
 echo "gitlab-runner version..."
 gitlab-runner --version
