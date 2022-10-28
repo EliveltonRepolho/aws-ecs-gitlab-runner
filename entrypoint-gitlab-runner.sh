e62720f2d133b0d9a4f3764d9185b1a20c6ed4a4
EliveltonRepolho <elivelton.repolho@gmail.com>
2022-10-28 02:18:26 -0300

    update new script

--- entrypoint-gitlab-runner.sh
+++ entrypoint-gitlab-runner.sh
@@ -32,7 +32,8 @@ GLOBAL_SECTION_CONFIG='/etc/gitlab-runner/config.toml'
 echo "Default config.toml..."
 cat ${GLOBAL_SECTION_CONFIG} 2> /dev/null
 
-# Override default config.toml
+# Global config file
+
 cat <<EOF >$GLOBAL_SECTION_CONFIG
 concurrent = ${RUNNER_CONCURRENT_LIMIT}
 check_interval = 0
@@ -42,10 +43,17 @@ check_interval = 0
   session_timeout = 1800
 EOF
 
-TEMPLATE_FILE_GENERAL='./template-general-config.toml'
-cat <<EOF >$TEMPLATE_FILE_GENERAL
+
+# Per runner config file
+function create_runner_config_file {
+  local runner_type=$1
+  local config_file=$2
+  local instance_type=$3
+  local is_spot=${4:-false}
+
+cat <<EOF >$config_file
 [[runners]]
-  name = "echope-erp-gitlab-runner-general"
+  name = "echope-erp-gitlab-runner-${runner_type}"
   description = "Gitlab Runner executing Pipeline Jobs in EC2" 
   executor = "docker+machine"
   limit = ${RUNNER_CONCURRENT_LIMIT}
@@ -62,7 +70,7 @@ cat <<EOF >$TEMPLATE_FILE_GENERAL
     IdleTime = 60
     MaxBuilds = 10 # We delete the VM after N jobs has finished so we can try to evict running out of space (disk).
     MachineDriver = "amazonec2"
-    MachineName = "gitlab-general-%s"
+    MachineName = "gitlab-${runner_type}-%s"
     MachineOptions = [
       "amazonec2-ami=${AWS_AMI}",
       "amazonec2-root-size=${AWS_ROOT_SIZE}",
@@ -73,8 +81,9 @@ cat <<EOF >$TEMPLATE_FILE_GENERAL
       "amazonec2-use-private-address=true",
       "amazonec2-ssh-user=${AWS_SSH_USER}",
       "amazonec2-security-group=${AWS_SECURITY_GROUP}",
-      "amazonec2-instance-type=${AWS_INSTANCE_TYPE_GENERAL}",
-      "amazonec2-tags=stack,echope-erp,stack-env,echope-erp-infra-devops,stack-group,echope-erp-gitlab-ec2-runner",
+      "amazonec2-instance-type=${instance_type}",
+      "amazonec2-request-spot-instance=${is_spot}",
+      "amazonec2-tags=stack,echope-erp,stack-env,echope-erp-infra-devops,stack-group,echope-erp-gitlab-ec2-runner-${runner_type}",
     ]
   [runners.cache]
     Type = "${CACHE_TYPE}"
@@ -86,65 +95,65 @@ cat <<EOF >$TEMPLATE_FILE_GENERAL
       BucketName = "${CACHE_S3_BUCKET_NAME}"
       BucketLocation = "${CACHE_S3_BUCKET_LOCATION}"
 EOF
+}
 
-TEMPLATE_FILE_IT='./template-it-config.toml'
-cat <<EOF >$TEMPLATE_FILE_IT
-[[runners]]
-  name = "echope-erp-gitlab-runner-integration-tests"
-  description = "Gitlab Runner executing Pipeline Jobs in EC2 with Integration Tests configuration" 
-  executor = "docker+machine"
-  limit = ${RUNNER_CONCURRENT_LIMIT}
-  request_concurrency = ${RUNNER_CONCURRENT_LIMIT}
-  environment = [
-    "DOCKER_DRIVER=overlay2",
-    "DOCKER_TLS_CERTDIR="
-  ]
-  [runners.docker]
-    privileged = true
-    disable_cache = true
-    tls_verify = true
-  [runners.machine]
-    IdleTime = 60
-    MaxBuilds = 10 # We delete the VM after N jobs has finished so we can try to evict running out of space (disk).
-    MachineDriver = "amazonec2"
-    MachineName = "gitlab-it-%s"
-    MachineOptions = [
-      "amazonec2-ami=${AWS_AMI}",
-      "amazonec2-root-size=${AWS_ROOT_SIZE}",
-      "amazonec2-region=${AWS_DEFAULT_REGION}",
-      "amazonec2-vpc-id=${AWS_VPC_ID}",
-      "amazonec2-subnet-id=${AWS_SUBNET_ID}",
-      "amazonec2-zone=${AWS_SUBNET_ZONE}",
-      "amazonec2-use-private-address=true",
-      "amazonec2-ssh-user=${AWS_SSH_USER}",
-      "amazonec2-security-group=${AWS_SECURITY_GROUP}",
-      "amazonec2-instance-type=${AWS_INSTANCE_TYPE_IT}",
-      "amazonec2-tags=stack,echope-erp,stack-env,echope-erp-infra-devops,stack-group,echope-erp-gitlab-ec2-runner-it",
-    ]
-  [runners.cache]
-    Type = "${CACHE_TYPE}"
-    Shared = ${CACHE_SHARED}
-    [runners.cache.s3]
-      ServerAddress = "${CACHE_S3_SERVER_ADDRESS}"
-      AccessKey = "${CACHE_S3_ACCESS_KEY}"
-      SecretKey = "${CACHE_S3_SECRET_KEY}"
-      BucketName = "${CACHE_S3_BUCKET_NAME}"
-      BucketLocation = "${CACHE_S3_BUCKET_LOCATION}"
-EOF
+TEMPLATE_FILE_GENERAL='./template-general-config.toml'
+create_runner_config_file "general" ${TEMPLATE_FILE_GENERAL} ${AWS_INSTANCE_TYPE_GENERAL} "false"
+
+TEMPLATE_FILE_GENERAL_SPOT='./template-general-spot-config.toml'
+create_runner_config_file "general" ${TEMPLATE_FILE_GENERAL_SPOT} ${AWS_INSTANCE_TYPE_GENERAL} "true"
+
+TEMPLATE_FILE_MEDIUM='./template-medium-config.toml'
+create_runner_config_file "medium" ${TEMPLATE_FILE_MEDIUM} ${AWS_INSTANCE_TYPE_MEDIUM} "false"
+
+TEMPLATE_FILE_MEDIUM_SPOT='./template-medium-spot-config.toml'
+create_runner_config_file "medium" ${TEMPLATE_FILE_MEDIUM_SPOT} ${AWS_INSTANCE_TYPE_MEDIUM} "true"
+
+TEMPLATE_FILE_LARGE='./template-large-config.toml'
+create_runner_config_file "large" ${TEMPLATE_FILE_LARGE} ${AWS_INSTANCE_TYPE_LARGE} "false"
+
+TEMPLATE_FILE_LARGE_SPOT='./template-large-spot-config.toml'
+create_runner_config_file "large" ${TEMPLATE_FILE_LARGE_SPOT} ${AWS_INSTANCE_TYPE_LARGE} "true"
+
+# Register runners
+# --debug
 
 echo "Registering runner using config.toml template file: $TEMPLATE_FILE_GENERAL"
-
-# --debug
 gitlab-runner --debug register \
 --template-config $TEMPLATE_FILE_GENERAL \
 --non-interactive \
---run-untagged
+--run-untagged \
+--tag-list "aws:small,aws:general"
 
-echo "Registering runner using config.toml template file: $TEMPLATE_FILE_IT"
+echo "Registering runner using config.toml template file: $TEMPLATE_FILE_GENERAL_SPOT"
 gitlab-runner --debug register \
---template-config $TEMPLATE_FILE_IT \
+--template-config $TEMPLATE_FILE_GENERAL_SPOT \
 --non-interactive \
---tag-list "test:integration:browser"
+--tag-list "aws:small:spot,aws:general:spot"
+
+echo "Registering runner using config.toml template file: $TEMPLATE_FILE_MEDIUM"
+gitlab-runner --debug register \
+--template-config $TEMPLATE_FILE_MEDIUM \
+--non-interactive \
+--tag-list "aws:medium"
+
+echo "Registering runner using config.toml template file: $TEMPLATE_FILE_MEDIUM_SPOT"
+gitlab-runner --debug register \
+--template-config $TEMPLATE_FILE_MEDIUM_SPOT \
+--non-interactive \
+--tag-list "aws:medium:spot"
+
+echo "Registering runner using config.toml template file: $TEMPLATE_FILE_LARGE"
+gitlab-runner --debug register \
+--template-config $TEMPLATE_FILE_LARGE \
+--non-interactive \
+--tag-list "aws:large"
+
+echo "Registering runner using config.toml template file: $TEMPLATE_FILE_LARGE_SPOT"
+gitlab-runner --debug register \
+--template-config $TEMPLATE_FILE_LARGE_SPOT \
+--non-interactive \
+--tag-list "aws:large:spot"
 
 echo "gitlab-runner version..."
 gitlab-runner --version
