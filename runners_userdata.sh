393b13969d76c0730043b6d260b505892252356f
Elivelton Repolho <elivelton.repolho@gmail.com>
2023-07-12 12:58:58 -0300

    Update runners_userdata.sh

--- runners_userdata.sh
+++ runners_userdata.sh
@@ -2,3 +2,12 @@
 
 # https://gitlab.com/gitlab-org/gitlab/-/issues/390385#note_1265442367
 mkdir -p /etc/docker
+
+
+echo "Create swap partition"
+# https://repost.aws/knowledge-center/ec2-memory-swap-file
+dd if=/dev/zero of=/swapfile bs=128M count=32
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+swapon -s
